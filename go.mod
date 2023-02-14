5c33cbdd8c253838dde039f87a9b455f6197008f
diwq <diwq@isyscore.com>
2023-02-14 15:29:51 +0800

    add Reverse: for v0.1.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xiaoqingqing681/goFirst
+
+go 1.18
--- stringutil/reverse_test.go
+++ stringutil/reverse_test.go
@@ -1,1 +1,20 @@
 package stringutil
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	for _, c := range []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	} {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+//如何开源自己的 Go 库   https://blog.51cto.com/u_15289640/5842814
